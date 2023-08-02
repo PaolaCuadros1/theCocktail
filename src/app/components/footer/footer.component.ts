71866d94c577322c4b37a1c114c3ee1798d43779
PaolaCuadros1 <paola.cuadroso@gmail.com>
2023-08-02 15:50:59 -0500

    Created footer component.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'footer',
+  selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  letters: string = ''
   constructor() { }
 
   ngOnInit(): void {
+    this.generateAbecesary()
+  }
+
+  generateAbecesary() {
+    for (let i = 65; i <= 90; i++) {
+      const slash = (i == 65 || i == 91) ? '' : '/'
+      this.letters = `${this.letters}${slash}${String.fromCharCode(i)}`
+    }
   }
 
 }
