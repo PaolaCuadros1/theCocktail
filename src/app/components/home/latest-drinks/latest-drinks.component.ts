9a2d4ecadabc06e874fdfb723871aeac047f42d1
PaolaCuadros1 <paola.cuadroso@gmail.com>
2023-08-02 14:57:03 -0500

    Created latest drinks component.

---
+++ src/app/components/home/latest-drinks/latest-drinks.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'latest-drinks',
+  templateUrl: './latest-drinks.component.html',
+  styleUrls: ['./latest-drinks.component.scss']
+})
+export class LatestDrinksComponent implements OnInit {
+
+  popularDrinks = [
+    { img: 'assets/images/Cocktail-Horses-Neck.png', name: 'Cocktail Horse’s Neck' },
+    { img: 'assets/images/Planters-Punch.png', name: 'Planter’s Punch' },
+    { img: 'assets/images/Ramos-Gin-Fizz.png', name: 'Ramos Gin Fizz' },
+    { img: 'assets/images/Gin-Lemon.png', name: 'Gin Lemon' },
+    { img: 'assets/images/Gin-Tonic.png', name: 'Gin Tonic' },
+    { img: 'assets/images/Vodka-Tonic.png', name: 'Vodka Tonic' },
+    { img: 'assets/images/Vodka-Lemon.png', name: 'Vodka Lemon' },
+    { img: 'assets/images/Vodka-Slime.png', name: 'Vodka Slime' },
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
