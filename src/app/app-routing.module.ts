59fe841743888b00f88e38a2781889b74497b031
PaolaCuadros1 <paola.cuadroso@gmail.com>
2023-08-02 21:47:18 -0500

    Added menu component to link of header and the routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'menu', component: MenuComponent}
 ];
 
 @NgModule({
