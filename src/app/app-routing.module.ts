c91eee7fb917db93df2ed374132101bfbc4841c6
PaolaCuadros1 <paola.cuadroso@gmail.com>
2023-08-04 09:08:39 -0500

    Added route to print ingretients of cocktail.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { MenuComponent } from './components/cocktails/cocktails.component';
+import { IngredientsComponent } from './components/ingredients/ingredients.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
   {path: 'cocktails', component: MenuComponent},
   {path: 'cocktails/:letter', component: MenuComponent},
+  {path: 'cocktail/:id', component: IngredientsComponent},
 ];
 
 @NgModule({
